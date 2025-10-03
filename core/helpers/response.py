f8375f37722c7f8ec72c0a1e078ceea3351b56ff
pedrocarvajal <pedro@memeonlyme.com>
2025-10-03 12:51:39 +0400

    Refactor response helper to streamline error and success message handling
    - Consolidated message assignment for error and success responses in the `response` function.
    - Removed redundant conditional checks for message and data parameters to simplify the code structure.

--- core/helpers/response.py
+++ core/helpers/response.py
@@ -7,21 +7,13 @@ def response(message=None, data=None, status_code=200, **kwargs):
     if status_code >= 400:
         response_data["success"] = False
         response_data["code"] = status_code
-
-        if message:
-            response_data["message"] = message
-        if data:
-            response_data["error"] = data
-        
+        response_data["message"] = message
+        response_data["error"] = data
         response_data.update(kwargs)
     else:
         response_data["success"] = True
-        
-        if message:
-            response_data["message"] = message
-        if data:
-            response_data["data"] = data
-        
+        response_data["message"] = message
+        response_data["data"] = data
         response_data.update(kwargs)
 
     return JsonResponse(response_data, status=status_code)
