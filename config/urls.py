f0b2d43a4867e875dc37c4c704600ba9c4ef9b72
pedrocarvajal <pedro@memeonlyme.com>
2025-10-08 10:13:32 +0400

    Update URL configuration to append media file serving
    - Modified urlpatterns to append the static URL configuration for serving media files, ensuring it is included in all environments.

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("api/", include("core.routes.api")),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
